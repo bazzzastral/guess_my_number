429d176d0631a80bb9618d68712e1b47c22fc3fc
Marek <marekfirlit.biz@gmail.com>
2022-06-12 14:44:07 +0100

    Response for empty input field above Check button

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 'use strict';
-/*
-console.log(document.querySelector('.message').textContent);
-
-document.querySelector('.message').textContent = 'Correct Number!';
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
-
-document.querySelector('.guess').value = 23; //nadpisywanie input elementu
-console.log(document.querySelector('.guess').value);
-*/
 
 document.querySelector('.check').addEventListener('click', function () {
-  console.log(document.querySelector('.guess').value);
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  // Guess input is empty and response
+  if (!guess) {
+    document.querySelector('.message').textContent = ' No number!';
+  }
 });
