bfdbc7d15a682a81844c8a26a639f4f7955ecce2
Marek <marekfirlit.biz@gmail.com>
2022-06-12 15:00:53 +0100

    Updated response for user input

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 'use strict';
+//Secret number
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
-  // Guess input is empty and response
+  // Response for value passed by user
   if (!guess) {
-    document.querySelector('.message').textContent = ' No number!';
+    document.querySelector('.message').textContent = 'No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = 'Too High!';
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = 'Too low!';
   }
 });
