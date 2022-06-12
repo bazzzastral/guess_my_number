e179cf402ec2e1b84e72392dfa019f919b0482a5
Marek <marekfirlit.biz@gmail.com>
2022-06-12 15:25:57 +0100

    Changed UI when win

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 'use strict';
+
 //Secret number
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
@@ -8,11 +9,17 @@ document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
-  // Response for value passed by user
+  //When is no input
   if (!guess) {
     document.querySelector('.message').textContent = 'No number!';
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number!';
+
+    //Change UI when user win
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    //When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too High!';
@@ -22,6 +29,8 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = 'You lost the game!';
       document.querySelector('.score').textContent = 0;
     }
+
+    //when guess is too low
   } else if (guess < secretNumber) {
     document.querySelector('.message').textContent = 'Too low!';
     score--;
