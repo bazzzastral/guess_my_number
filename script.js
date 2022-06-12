4a3a7a73ee9af2b8f39630a544bbbe688436ee05
Marek <marekfirlit.biz@gmail.com>
2022-06-12 15:15:57 +0100

    Updated game logic

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 'use strict';
 //Secret number
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
 document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
@@ -13,8 +14,25 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number!';
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = 'Too High!';
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too High!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
   } else if (guess < secretNumber) {
     document.querySelector('.message').textContent = 'Too low!';
+    score--;
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too High!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+    document.querySelector('.score').textContent = score;
   }
 });
